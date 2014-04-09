81565fa77552aa8ea85329ea05f3663b94224daa
reedfim <reedfim@gmail.com>
2014-04-09 21:51:45 +0900

    studying... # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is up-to-date with 'origin/master'. # # Changes to be committed: #	new file:   public/js/main.js #	modified:   views/index.ejs #

--- public/js/main.js
+++ public/js/main.js
@@ -3,28 +3,48 @@
  * */
 
 //네임스페이스를 myApp으로 설정
-var myAppModule = angular.module('myApp', []);
+var myAppModule = angular.module('myApp', ['utility']);
+
+var utility = angular.module('utility', []);
+utility.filter('titleCase', function(){
+    var titleCaseFilter = function(input){
+        var words = input.split(' ');
+        for( var i=0; i<words.length; i++){
+            words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+        }
+        return words.join(' ');
+    };
+
+    return titleCaseFilter;
+})
+
+myAppModule.factory('Items', function(){
+    var items = [
+        {
+            title : '페인트 그릇',
+            quantity : 18,
+            price : 3.95
+        },
+        {
+            title : '땡땡이 리본',
+            quantity : 121,
+            price : 11.7
+        },
+        {
+            title : '공깃돌',
+            quantity : 2,
+            price : 5.2
+        }
+    ];
+
+    return items;
+
+});
 
 //myApp네임스페이스를 사용하는 모듈 작성, 모듈의 특성은 컨트롤러
 myAppModule.controller('CartController',
-    function($scope){
-        $scope.items = [
-            {
-                title : '페인트 그릇',
-                quantity : 8,
-                price : 3.95
-            },
-            {
-                title : '땡땡이 리본',
-                quantity : 11,
-                price : 11.7
-            },
-            {
-                title : '공깃돌',
-                quantity : 2,
-                price : 5.2
-            }
-        ];
+    function($scope, Items){
+        $scope.items = Items;
 
         $scope.remove = function(index){
             $scope.items.splice(index, 1);
@@ -42,7 +62,7 @@ myAppModule.controller('SomeController',
             startingEstimate : 0
         };
 
-        var computeNeed = function(){
+        var computeNeed = function(newValue, oldValue, scope){
             console.log($scope.funding.startingEstimate);
             var estimate = parseInt($scope.funding.startingEstimate, 10);
             if($scope.funding.startingEstimate === ''){
@@ -54,6 +74,8 @@ myAppModule.controller('SomeController',
         $scope.requestFunding = function(){
             alert('아직 앙돼요~');
         };
+
+        $scope.testTitle = 'hi hello';
 
         $scope.$watch('funding.startingEstimate', computeNeed);
     });
